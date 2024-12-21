df61084e2e6ec902a762bd2caa32a20b3c9f6aee
Katie Winner <katelyn.blomquist@gmail.com>
2024-12-21 17:46:51 -0500

    get button working

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,4 +1,5 @@
-import { IconArrowRight, IconBrandTelegram } from "@tabler/icons-react";
+import { IconArrowRight, IconBrandTelegram, IconSend } from "@tabler/icons-react";
+import styles from "./button.module.css";
 
 export default function Button(props: { text: string, action: () => void, variant: number, icon: number }) {
 
@@ -18,7 +19,7 @@ export default function Button(props: { text: string, action: () => void, varian
         } 
         if(icon === 2){
             return(
-                <IconBrandTelegram />
+                <IconSend />
             )
         }
         return (
@@ -29,18 +30,18 @@ export default function Button(props: { text: string, action: () => void, varian
     function buildButton() {
         if (variant === 1) {
             return (
-                <div onClick = {buttonAction}>
+                <div className={styles.primary} onClick = {buttonAction}>
                     <p>{text}</p>
-                    <div>{addIcon()}</div>
+                    <div className={styles.icon}>{addIcon()}</div>
                 </div>
             )
         } 
         
         if (variant === 2) {
             return (
-                <div>
+                <div className={styles.secondary} onClick={buttonAction}>
                     <p>{text}</p>
-                    <div>{addIcon()}</div>
+                    <div className={styles.icon}>{addIcon()}</div>
                 </div>
             )
         }
