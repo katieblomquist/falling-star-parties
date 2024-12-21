3e906abb5880a653eb0163230509024ee9a9569e
Katie Winner <katelyn.blomquist@gmail.com>
2024-12-21 17:09:53 -0500

    update structure

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,1 +1,56 @@
-export default function Button() {}
+import { IconArrowRight, IconBrandTelegram } from "@tabler/icons-react";
+
+export default function Button(props: { text: string, action: () => void, variant: number, icon: number }) {
+
+    const variant = props.variant;
+    const text = props.text
+    const icon = props.icon;
+
+    const buttonAction = () => {
+        props.action();
+    }
+
+    function addIcon() {
+        if(icon === 1){
+            return(
+                <IconArrowRight />
+            )
+        } 
+        if(icon === 2){
+            return(
+                <IconBrandTelegram />
+            )
+        }
+        return (
+            null
+        )
+    }
+
+    function buildButton() {
+        if (variant === 1) {
+            return (
+                <div onClick = {buttonAction}>
+                    <p>{text}</p>
+                    <div>{addIcon()}</div>
+                </div>
+            )
+        } 
+        
+        if (variant === 2) {
+            return (
+                <div>
+                    <p>{text}</p>
+                    <div>{addIcon()}</div>
+                </div>
+            )
+        }
+
+        return (
+            null
+        )
+    }
+
+    return (
+        buildButton()
+    )
+}
