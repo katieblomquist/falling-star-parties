871dd02a1076801b8fe9a26a1746d80a0e2d5acf
Katie Winner <katelyn.blomquist@gmail.com>
2025-01-06 08:48:50 -0500

    create text input

--- src/components/form/Text Input/textInput.tsx
+++ src/components/form/Text Input/textInput.tsx
@@ -1,1 +1,43 @@
-export default function TextInput() {}
+import { useState } from "react";
+import styles from "./textInput.module.css";
+
+export default function TextInput(props: { type: string, placeholder: string, required: boolean, id: string, label: string, input: string, setInput: (data: string) => void }) {
+
+    const type = props.type;
+    const placeholder = props.placeholder;
+    const required = props.required;
+    const id = props.id;
+    const label = props.label;
+
+    const htmlInput = document.getElementById(id);
+    htmlInput?.addEventListener("change", updateValue);
+
+    function updateValue(e: any) {
+        props.setInput(e.target.value);
+    }
+
+    function buildInput() {
+        if (required) {
+            return (
+                <>
+                    <div className={styles.textInput}>
+                        <label>{label}</label>
+                        <input className={styles.input} type={type} id={id} name={id} placeholder={placeholder} required></input>
+                    </div>
+
+                </>
+            )
+
+        }
+        return (
+            <>
+                <label>{label}</label>
+                <input type={type} id={id} name={id} placeholder={placeholder}></input>
+            </>
+        );
+    }
+
+    return (
+        buildInput()
+    )
+}
