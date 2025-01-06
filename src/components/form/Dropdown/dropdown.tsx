79f9ba09dc2f6023b9788271f2e2f2505fbed3af
Katie Winner <katelyn.blomquist@gmail.com>
2025-01-06 08:49:19 -0500

    create dropdown

--- src/components/form/Dropdown/dropdown.tsx
+++ src/components/form/Dropdown/dropdown.tsx
@@ -1,21 +1,24 @@
-import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
+'use client';
+
 import { useState } from "react";
+import { IconChevronDown, IconChevronUp } from "@tabler/icons-react";
 import styles from "./dropdown.module.css";
 
 export default function Dropdown(props: { options: string[], selected: number, setData: (data: number) => void }) {
 
     const options = props.options;
-    const [selected, setSelected] = useState(props.selected);
+    const selected = props.selected;
     const [toggled, setToggle] = useState(false);
 
     function handleSelected(selected: number) {
-        setSelected(selected);
-        props.setData(selected);
+        // TODO(ningy says) remove the internal state for selected and rely on passing data down from and up to parent component
         setToggle(false);
+        props.setData(selected);
+        
     }
 
-    function handleToggle(){
-        if(toggled){
+    function handleToggle() {
+        if (toggled) {
             setToggle(false);
         } else {
             setToggle(true)
@@ -24,27 +27,27 @@ export default function Dropdown(props: { options: string[], selected: number, s
 
     return (
         <>
-        <div className={styles.dropdown}>
-            <div className={styles.selected} onClick={handleToggle}>
-                <p>{options[selected]}</p>
-                <div className={styles.chevron}>
-                    {toggled ? (
-                        <IconChevronUp />
-                    ) : (
-                        <IconChevronDown />
-                    )}
-                </div>
-            </div >
+            <div className={styles.dropdown}>
+                <div className={styles.selected} onClick={handleToggle}>
+                    <p>{options[selected]}</p>
+                    <div className={styles.chevron}>
+                        {toggled ? (
+                            <IconChevronUp />
+                        ) : (
+                            <IconChevronDown />
+                        )}
+                    </div>
+                </div >
                 {toggled ? (
                     <div className={styles.options}>
                         {options.map((option, index) => {
-                            return(
+                            return (
                                 <p className={styles.option} onClick={() => handleSelected(index)}>{option}</p>
                             )
                         })}
                     </div>
-                ): null}
-        </div>
+                ) : null}
+            </div>
         </>
     )
 }
