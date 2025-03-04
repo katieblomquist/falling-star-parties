c18165eaa030d4dc3921fedc2b3c5d8322f78fe3
Katie Winner <katelyn.blomquist@gmail.com>
2025-03-04 17:19:32 -0500

    Refactor Select cards

--- src/components/form/Selection Cards/selectionCard.tsx
+++ src/components/form/Selection Cards/selectionCard.tsx
@@ -2,56 +2,45 @@ import { SelectionCardContent } from "@/app/book/page";
 import { IconCircle, IconCircleFilled, IconClock } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 import styles from "./selectionCard.module.css";
+import HorizontalCard from "./horizontalCard";
+import VerticleCard from "./verticleCard";
 
 export default function SelectionCard(props: { type: number, content: SelectionCardContent, selected: boolean, makeSelection: (id: number, selected: boolean) => void }) {
-    
+
     const type = props.type;
     const content = props.content;
     const selected = props.selected;
 
-    function handleClick(){
-        if(selected){
+    function handleClick() {
+        if (selected) {
             props.makeSelection(content.id, false);
         } else {
             props.makeSelection(content.id, true);
         }
-        
+
     }
 
     function buildCard() {
         if (type === 0) {
             return (
-                <div className={styles[`rectangle${selected ? "Active": ""}`]} onClick={handleClick}>
-                    <div className={styles.header}>
-                        <div className={styles.selectionName}>
-                            {selected ? (
-                                <IconCircleFilled stroke={1} color="#343B95" />
-                            ) : (<IconCircle stroke={1} />)}
-                            <h5>{content.title}</h5>
-                        </div>
-
-                        <div className={styles.time}>
-                            <IconClock stroke={1} />
-                            <p>{content.duration}</p>
-                        </div>
-                    </div>
-                    <div className={styles.content}>
-                        <p>{content.description}</p>
-                    </div>
-                </div>
-            )
-        } else if (type === 1) {
-            return (
-                <div className={styles[`square${selected ? "Active" : ""}`]} onClick={handleClick}>
+                <div className={styles[`card${selected ? "Active" : ""}`]} onClick={handleClick}>
                     <div className={styles.selectionName}>
                         {selected ? (
                             <IconCircleFilled stroke={1} color="#343B95" />
                         ) : (<IconCircle stroke={1} />)}
-                        <h5>{content.title}</h5>
                     </div>
-                    <div>
-                        <p className={styles.content}>{content.description}</p>
+                    <HorizontalCard content={content} />
+                </div>
+            )
+        } else if (type === 1) {
+            return (
+                <div className={styles[`card${selected ? "Active" : ""}`]} onClick={handleClick}>
+                    <div className={styles.selectionName}>
+                        {selected ? (
+                            <IconCircleFilled stroke={1} color="#343B95" />
+                        ) : (<IconCircle stroke={1} />)}
                     </div>
+                    <VerticleCard content={content} />
                 </div>
             )
         } else {
---
+++ src/components/form/Selection Cards/verticleCard.tsx
@@ -0,0 +1,20 @@
+import { SelectionCardContent } from "@/app/book/page";
+import { IconClock } from "@tabler/icons-react";
+import styles from "./verticleCard.module.css";
+
+export default function VerticleCard(props: { content: SelectionCardContent }) {
+
+
+    return (
+        <>
+        <div className={styles.card}>
+            <div className={styles.header}>
+                <h5>{props.content.title}</h5>
+            </div>
+            <div className={styles.content}>
+                <p>{props.content.description}</p>
+            </div>
+        </div>
+        </>
+    )
+}
