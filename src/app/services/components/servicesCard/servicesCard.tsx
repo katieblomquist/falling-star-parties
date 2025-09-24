f48be0a2d02b949448bf29b54c7258cc8bd01b60
Katie Winner <katelyn.blomquist@gmail.com>
2025-09-24 05:45:22 -0400

    start new services page

---
+++ src/app/services/components/servicesCard/servicesCard.tsx
@@ -0,0 +1,11 @@
+ 
+
+ export default function ServicesCard(props: {primary: boolean, title: String, time: String, activities: Array<String>, basePrice: Number, addCharacter: Number}){
+
+
+    return(
+        <>
+        
+        </>
+    )
+ }
