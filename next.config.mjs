dbb60677940fe56c033ff5f132839cdab8e78406
Katie Winner <katelyn.blomquist@gmail.com>
2026-02-12 20:42:40 -0500

    cont. debug no notion key

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    runtime: 'nodejs',
+  },
+  env: {
+    NOTION_KEY: process.env.NOTION_KEY,
+    NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
+  }
+};
 
 export default nextConfig;
