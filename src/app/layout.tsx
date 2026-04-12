765b1da6a35b400e7e8b62752f68954f50ea12fa
Katie Winner <katelyn.blomquist@gmail.com>
2026-04-12 06:56:43 -0400

    improve booking form mobile styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -161,6 +161,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={dhyana.className}>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
