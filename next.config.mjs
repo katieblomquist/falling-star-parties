d817bdefa5d6f97b5a62a7734eb0b4969602e756
Katie Winner <katelyn.blomquist@gmail.com>
2026-05-10 10:16:06 -0400

    another try

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@react-pdf/renderer"],
-  experimental: {
-    serverComponentsExternalPackages: ["@react-pdf/renderer"],
-  },
 };
-
 export default nextConfig;
